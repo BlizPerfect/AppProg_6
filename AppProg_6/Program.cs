61c6aa80e243d4f7d53e4744ad1d350c4c6bee8d
BlizPerfect <zax_2000@mail.ru>
2021-12-19 18:52:45 +0500

    Реализован основной функционал.
    По нажатию кнопки, пользователь вытаскивает бочонок со случайным номером.

---
+++ AppProg_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppProg_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var roulette = new Roulette();
+            while (!roulette.IsEmpty)
+            {
+                Console.ReadKey();
+                roulette.PullOutKeg();
+            }
+        }
+    }
+}
---
+++ AppProg_6/Roulette.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Roulette
+{
+    private int NumberOfKegs;
+    private List<int> Kegs;
+    private Random Rnd = new Random();
+    public bool IsEmpty { get; private set; }
+    public Roulette()
+    {
+        NumberOfKegs = InputCount();
+        IsEmpty = false;
+        Kegs = CreateKegsMassive(NumberOfKegs);
+        Console.WriteLine("Для вытягивания бочонка нажимайте любую кнопку.\n");
+    }
+
+    private bool CheckRemainderOfKegs()
+    {
+        if (Kegs.Count == 0)
+        {
+            Console.WriteLine("Бочонки закончились!");
+            return true;
+        }
+        return false;
+    }
+
+    public void PullOutKeg()
+    {
+        var index = Rnd.Next(0, Kegs.Count);
+        var keg = Kegs[index];
+        Console.WriteLine("Вы вытащили бочонок под номером: " + keg);
+        DeleteKegFromKegs(index);
+        IsEmpty = CheckRemainderOfKegs();
+    }
+
+    private void DeleteKegFromKegs(int index)
+    {
+        Kegs.RemoveAt(index);
+    }
+
+    private List<int> CreateKegsMassive(int count)
+    {
+        var result = new List<int>();
+        for (var i = 0; i < count; i++)
+        {
+            result.Add(i + 1);
+        }
+        return result;
+    }
+
+    private int InputCount()
+    {
+        var error = true;
+        int result = 0;
+        Console.Write("Введите число бочонков: ");
+        while (error)
+        {
+            if (int.TryParse(Console.ReadLine(), out result))
+            {
+                if (result > 0)
+                {
+                    error = false;
+                }
+                else
+                {
+                    Console.Write("Нужно ввести положительное число, большее 0, попробуйте снова: ");
+                }
+            }
+            else
+            {
+                Console.Write("Вы ввели не число, попробуйте снова: ");
+            }
+        }
+        return result;
+    }
+}
